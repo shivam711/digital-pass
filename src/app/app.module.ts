7d54c47ee849abdaf073880604b3f87235bff083
shivam711 <sshrivastava711@gmail.com>
2018-04-24 13:08:14 +0530

    commited
    yeah

---
+++ src/app/app.module.ts
@@ -0,0 +1,78 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { HttpClientModule } from '@angular/common/http';
+
+import { File } from '@ionic-native/file';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+import { Transfer } from '@ionic-native/transfer';
+import { FilePath } from '@ionic-native/file-path';
+import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer';
+
+import { UploadComponent } from '../components/upload/upload';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+import { RegisterPage } from '../pages/register/register';
+import { MenuPage } from '../pages/menu/menu';
+import { RenewPage } from '../pages/renew/renew';
+import { CreatePage } from '../pages/create/create';
+import { MainPage } from '../pages/main/main';
+import { StatusPage } from '../pages/status/status';
+import { QrcodePage } from '../pages/qrcode/qrcode';
+import { PaymentPage } from "../pages/payment/payment";
+import { UploadPage } from '../pages/upload/upload';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    LoginPage,
+    RegisterPage,
+    MenuPage,
+    UploadComponent,
+    RenewPage,
+    CreatePage,
+    MainPage,
+    StatusPage,
+    QrcodePage,
+    PaymentPage,
+    UploadPage
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    LoginPage,
+    RegisterPage,
+    MenuPage,
+    RenewPage,
+    CreatePage,
+    MainPage,
+    StatusPage,
+    QrcodePage,
+    PaymentPage,
+    UploadPage,
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    InAppBrowser,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    FileTransfer,
+    FileTransferObject,
+    File,
+    Transfer,
+    Camera,
+    FilePath
+  ]
+})
+export class AppModule {}
